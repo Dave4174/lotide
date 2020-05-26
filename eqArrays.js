5830bd8c0dcdae02d8a9a3689bf31de72e4c8f63
Dave4174 <dlang4174@gmail.com>
2020-05-26 20:10:49 +0000

    Implement eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let match = true;
+  if (array1.length !== array2.length) {
+    match = false;
+  } else {
+    array1.forEach(function(arrayItem, i){
+      if (arrayItem !== array2[i]) {
+        match = false;
+      }
+    });
+  }
+  return match;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
