295978beaeee47cb8a6a86627b103f9d2d777db1
Dave4174 <dlang4174@gmail.com>
2020-05-26 20:44:18 +0000

    assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let match = true;
+  if (array1.length !== array2.length) {
+    match = false;
+  } else {
+    array1.forEach(function(arrayItem, i) {
+      if (arrayItem !== array2[i]) {
+        match = false;
+      }
+    });
+  }
+  return match;
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
