6b5b6c0f8ce96db35e05483e0f94362af0a9a090
Dave4174 <dlang4174@gmail.com>
2020-06-01 17:30:02 +0000

    Refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
@@ -6,21 +8,18 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(array1, array2) {
-  let match = true;
-  if (array1.length !== array2.length) {
-    match = false;
-  } else {
-    array1.forEach(function(arrayItem, i) {
-      if (arrayItem !== array2[i]) {
-        match = false;
-      }
-    });
-  }
-  return match;
-};
+// const eqArrays = function(array1, array2) {
+//   let match = true;
+//   if (array1.length !== array2.length) {
+//     match = false;
+//   } else {
+//     array1.forEach(function(arrayItem, i) {
+//       if (arrayItem !== array2[i]) {
+//         match = false;
+//       }
+//     });
+//   }
+//   return match;
+// };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
